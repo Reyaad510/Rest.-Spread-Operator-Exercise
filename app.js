28d16fab418ae881990920815a10af846712e42c
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-15 20:57:19 -0500

    filterOutOdds refactored to ES2015

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hi");
+// Refactor Below Function to ES2015
+/*
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function(num) {
+      return num % 2 === 0
+    });
+  }
+  */
+
+const filterOutOdds = (...arg) => {
+  let nums = [...arg];
+  return nums.filter((num) => {
+    return num % 2 === 0;
+  });
+};
+
+console.log(filterOutOdds(1, 22, 17, 10, 192, 928732, 1213));

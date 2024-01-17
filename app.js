a201373ee753df7f93bb9bf47834a730bce31f95
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-16 21:31:35 -0500

    removeRandom completed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const filterOutOdds = (...arg) => {
   });
 };
 
-console.log(filterOutOdds(1, 22, 17, 10, 192, 928732, 1213));
+// console.log(filterOutOdds(1, 22, 17, 10, 192, 928732, 1213));
 
 // findMin
 
@@ -23,7 +23,7 @@ const findMin = (...args) => {
   return Math.min(...args);
 };
 
-console.log(findMin(89, 30, 200, 21, 8, 29));
+// console.log(findMin(89, 30, 200, 21, 8, 29));
 
 // mergeObjects
 
@@ -31,7 +31,7 @@ const mergeObjects = (obj1, obj2) => {
   return { ...obj1, ...obj2 };
 };
 
-console.log(mergeObjects({ a: 1, b: 2 }, { c: 3, d: 4 }));
+// console.log(mergeObjects({ a: 1, b: 2 }, { c: 3, d: 4 }));
 
 // doubleAndReturnArgs
 
@@ -45,7 +45,38 @@ const doubleAndReturnArgs = (arr, ...args) => {
   return [...arr, ...doubled];
 };
 
-console.log(doubleAndReturnArgs([1, 2, 3], 4, 4));
-console.log(doubleAndReturnArgs([2], 10, 4));
+// console.log(doubleAndReturnArgs([1, 2, 3], 4, 4));
+// console.log(doubleAndReturnArgs([2], 10, 4));
 
 // Slice and Dice!
+
+/** remove a random element in the items array
+and return a new array without that item. */
+// creates new array including elements before and after the randomNum index
+const removeRandom = (items) => {
+  let randomNum = Math.floor(Math.random() * items.length);
+  return [...items.slice(0, randomNum), ...items.slice(randomNum + 1)];
+};
+
+removeRandom([1, 2, 3, 4]);
+
+/** Return a new array with every item in array1 and array2. */
+
+function extend(array1, array2) {}
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+function addKeyVal(obj, key, val) {}
+
+/** Return a new object with a key removed. */
+
+function removeKey(obj, key) {}
+
+/** Combine two objects and return a new object. */
+
+function combine(obj1, obj2) {}
+
+/** Return a new object with a modified key and value. */
+
+function update(obj, key, val) {}
